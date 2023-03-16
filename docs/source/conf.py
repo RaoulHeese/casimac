d96764d0a73471ce79f01016649082906c5daaf5
RaoulHeese <raoul.heese@gmail.com>
2023-03-16 12:38:50 +0100

    version: 1.2.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Raoul Heese'
 author = 'Raoul Heese'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.3'
+release = '1.2.4'
 
 
 # -- General configuration ---------------------------------------------------
